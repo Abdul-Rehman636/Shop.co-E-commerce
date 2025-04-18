16a134e65ae1e0eeb0cac6644086c8fe73ec2aed
Abdul-Rehman636 <rehmankk636@gmail.com>
2025-04-18 23:36:35 +0500

    Completed Hero Section.

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -22,19 +22,19 @@ const Hero = () => {
             className="w-[56px] h-[56px] animate-[spin_4s_linear_infinite]"
           />
         </div>
-        <div className="w-fit lg:pt-24 md:pt-8 pt-10 lg:pb-28 md:pb-16">
-          <p className="w-fit text-[48px] md:text-[64px] font-bold font-integralBold text-[#000000] flex flex-col md:leading-[4.2rem] sm:leading-[3rem]">
+        <div className="w-fit lg:pt-24 md:pt-8 sm:pt-9 pt-6 lg:pb-28 md:pb-16">
+          <p className="w-fit text-[36px] min-[540px]:text-[48px] sm:text-[48px] md:text-[64px] font-bold font-integralBold text-[#000000] flex flex-col md:leading-[4.2rem] sm:leading-[3rem] min-[540px]:leading-[2.7rem] leading-[2.13rem]">
             FIND CLOTHES <span>THAT MATCHES</span> <span>YOUR STYLE</span>
           </p>
-          <p className="text-[16px] font-normal font-satoshi text-[#00000099] lg:w-[34rem] md:w-[90%] md:mt-6 mt-5">
+          <p className="min-[540px]:text-[16px] text-[14px] font-normal font-satoshi text-[#00000099] lg:w-[34rem] md:w-[90%] md:mt-6 sm:mt-5 min-[540px]:mt-[18px] mt-[16px]">
             Browse through our diverse range of meticulously crafted garments,
             designed to bring out your individuality and cater to your sense of
             style.
           </p>
-          <button className="text-[16px] font-medium font-satoshi text-white py-4 px-[4.5rem] rounded-[62px] bg-black mt-6 hover:bg-transparent hover:text-black border-2 border-black cursor-pointer transition-all duration-500">
+          <button className="text-[16px] font-medium font-satoshi text-white py-3 sm:py-4 sm:w-auto w-[99%] sm:px-[4.5rem] rounded-[62px] bg-black sm:mt-6 mt-[22px] hover:bg-transparent hover:text-black border-2 border-black cursor-pointer transition-all duration-500">
             Shop Now
           </button>
-          <div className="flex md:mt-[3.2rem] sm:mt-[2.4rem] lg:justify-start justify-between lg:w-auto md:w-[85%] sm:w-[94%] lg:mx-0 mx-auto xl:gap-8 lg:gap-5">
+          <div className="sm:flex hidden md:mt-[3.2rem] sm:mt-[2.4rem] lg:justify-start justify-between lg:w-auto md:w-[85%] sm:w-[94%] lg:mx-0 mx-auto xl:gap-8 lg:gap-5">
             <div className="flex flex-col lg:border-r lg:border-r-[#0000001A] xl:pr-9 lg:pr-6">
               <p className="text-[40px] text-black font-satoshiBold">
                 <CountUp end={200} duration={6} />+
@@ -62,6 +62,35 @@ const Hero = () => {
               </p>
             </div>
           </div>
+          <div className="flex flex-col items-center sm:hidden min-[540px]:mt-[2.4rem] mt-[1.4rem] w-[80%] mx-auto">
+            <div className="flex justify-between items-center w-full">
+              <div className="flex flex-col lg:border-r lg:border-r-[#0000001A] xl:pr-9 lg:pr-6">
+                <p className="sm:text-[40px] text-[24px] text-black font-satoshiBold">
+                  <CountUp end={200} duration={6} />+
+                </p>
+                <p className="sm:text-[16px] text-[12px] -mt-[3px] font-normal text-[#00000099] font-satoshi">
+                  International Brands
+                </p>
+              </div>
+              <div className="border border-[#0000001A] w-[52px] rotate-90"></div>
+              <div className="flex flex-col lg:border-r lg:border-r-[#0000001A] xl:pr-9 lg:pr-6">
+                <p className="sm:text-[40px] text-[24px] text-black font-satoshiBold">
+                  <CountUp end={2000} duration={6} />+
+                </p>
+                <p className="sm:text-[16px] text-[12px] -mt-[3px] font-normal text-[#00000099] font-satoshi">
+                  High-Quality Products
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col mt-4">
+              <p className="sm:text-[40px] text-[24px] text-black font-satoshiBold">
+                <CountUp end={30000} duration={6} />+
+              </p>
+              <p className="sm:text-[16px] text-[12px] -mt-[3px] font-normal text-[#00000099] font-satoshi">
+                Happy Customers
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="flex lg:hidden justify-center md:mt-0 sm:mt-2 relative w-full">
@@ -72,19 +101,19 @@ const Hero = () => {
             className="w-full object-cover"
           />
         </div>
-        <div className="flex justify-between absolute mt-20 w-[90%]">
-          <div className="mt-52">
+        <div className="flex justify-between absolute min-[540px]:mt-20 mt-12 w-[90%]">
+          <div className="min-[540px]:mt-52 mt-[90px]">
             <img
               src={smallBlink}
               alt="small-Blink"
-              className="w-[72px] h-[72px] animate-[spin_4s_linear_infinite]"
+              className="min-[540px]:w-[72px] min-[540px]:h-[72px] w-[44px] h-[44px] animate-[spin_4s_linear_infinite]"
             />
           </div>
           <div>
             <img
               src={largeBlink}
               alt="large-Blink"
-              className="w-[132px] h-[132px] animate-[spin_4s_linear_infinite]"
+              className="min-[540px]:w-[132px] min-[540px]:h-[132px] w-[76px] h-[76px] animate-[spin_4s_linear_infinite]"
             />
           </div>
         </div>
