027ee5c0e23bd35465163ce6f433809bf9298bd3
Abdul-Rehman636 <rehmankk636@gmail.com>
2025-04-20 21:22:25 +0500

    Started Footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/main/Footer/Footer";
 import Header from "./components/main/Header/Header";
 import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
